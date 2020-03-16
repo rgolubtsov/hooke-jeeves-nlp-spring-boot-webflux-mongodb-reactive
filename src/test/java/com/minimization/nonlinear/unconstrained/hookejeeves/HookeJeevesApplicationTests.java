263c64c9f29cc08b93a23444b863a8a823d87d78
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2020-03-16 13:05:30 +1100

    Add description-version-copyright-license banner.

--- src/test/java/com/minimization/nonlinear/unconstrained/hookejeeves/HookeJeevesApplicationTests.java
+++ src/test/java/com/minimization/nonlinear/unconstrained/hookejeeves/HookeJeevesApplicationTests.java
@@ -1,3 +1,15 @@
+/*
+ * src/test/java/com/minimization/nonlinear/unconstrained/hookejeeves/
+ * HookeJeevesApplicationTests.java
+ * ============================================================================
+ * The Hooke and Jeeves nonlinear unconstrained minimization algorithm.
+ * Microservice. Version 0.1
+ * ============================================================================
+ * Written by Radislav (Radicchio) Golubtsov, 2020
+ *
+ * (See the LICENSE file at the top of the source tree.)
+ */
+
 package com.minimization.nonlinear.unconstrained.hookejeeves;
 
 import org.springframework.boot.test.context.SpringBootTest;
