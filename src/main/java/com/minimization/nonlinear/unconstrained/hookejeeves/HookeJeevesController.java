442b7184411bd4f21ee733da0bb06b211faf606f
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2020-03-17 19:15:00 +1100

    Add REST method to store initial guess data in the database for the Rosenbrock objective function - for HTTP PUT requests.

--- src/main/java/com/minimization/nonlinear/unconstrained/hookejeeves/HookeJeevesController.java
+++ src/main/java/com/minimization/nonlinear/unconstrained/hookejeeves/HookeJeevesController.java
@@ -13,12 +13,26 @@
 package com.minimization.nonlinear.unconstrained.hookejeeves;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HookeJeevesController {
+    @PutMapping("store")
+    public void store_initial_guess_data(
+        @RequestParam(name="nvars",    defaultValue="2"   ) final String nvars,
+        @RequestParam(name="startpt0", defaultValue="-1.2") final String startpt0,
+        @RequestParam(name="startpt1", defaultValue="1.0" ) final String startpt1,
+        @RequestParam(name="rho",      defaultValue="0.5" ) final String rho) {
+
+        System.out.println("==> nvars="    + nvars    + "\n"
+                         + "==> startpt0=" + startpt0 + "\n"
+                         + "==> startpt1=" + startpt1 + "\n"
+                         + "==> rho="      + rho);
+    }
+
     @GetMapping("solve")
     public void solve_the_problem_get(
         @RequestParam(name="obj_func",
