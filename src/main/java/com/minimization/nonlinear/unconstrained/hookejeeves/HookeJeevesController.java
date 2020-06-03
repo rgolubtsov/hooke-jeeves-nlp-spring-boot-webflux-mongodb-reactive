55efa65dc3d02aaa673fd12ea20b2e33cf85092c
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2020-06-03 17:47:00 +1000

    (1) Add separate HTTP PUT REST method to store initial guess data in the database for the Woods objective function. (2) Rename REST URL mapping for the HTTP PUT REST method for the Rosenbrock objective function (store initial guess data) to distinct it from Woods-related requests. (3) Add helper method to put initial guess data to the database - to encapsulate functionality common to both PUT methods.

--- src/main/java/com/minimization/nonlinear/unconstrained/hookejeeves/HookeJeevesController.java
+++ src/main/java/com/minimization/nonlinear/unconstrained/hookejeeves/HookeJeevesController.java
@@ -23,7 +23,7 @@ import org.bson.Document;
 /** The controller class of the microservice. */
 @RestController
 public class HookeJeevesController {
-    @PutMapping("store")
+    @PutMapping("store-rosenbrock")
     public void store_initial_guess_data(
         @RequestParam(name="nvars",    defaultValue="2"   ) final String nvars,
         @RequestParam(name="startpt0", defaultValue="-1.2") final String startpt0,
@@ -35,13 +35,51 @@ public class HookeJeevesController {
                          + "==> startpt1=" + startpt1 + "\n"
                          + "==>      rho=" + rho);
 
+        // Putting initial guess data to the database.
+        _put_to_db(nvars, startpt0, startpt1, null, null, rho);
+    }
+
+    @PutMapping("store-woods")
+    public void store_initial_guess_data(
+        @RequestParam(name="nvars",    defaultValue="4"  ) final String nvars,
+        @RequestParam(name="startpt0", defaultValue="-3" ) final String startpt0,
+        @RequestParam(name="startpt1", defaultValue="-1" ) final String startpt1,
+        @RequestParam(name="startpt2", defaultValue="-3" ) final String startpt2,
+        @RequestParam(name="startpt3", defaultValue="-1" ) final String startpt3,
+        @RequestParam(name="rho",      defaultValue="0.6") final String rho) {
+
+        System.out.println("==>    nvars=" + nvars    + "\n"
+                         + "==> startpt0=" + startpt0 + "\n"
+                         + "==> startpt1=" + startpt1 + "\n"
+                         + "==> startpt2=" + startpt2 + "\n"
+                         + "==> startpt3=" + startpt3 + "\n"
+                         + "==>      rho=" + rho);
+
+        // Putting initial guess data to the database.
+        _put_to_db(nvars, startpt0, startpt1, startpt2, startpt3, rho);
+    }
+
+    // Helper method. Puts initial guess data to the database.
+    private void _put_to_db(final String nvars,
+                            final String startpt0,
+                            final String startpt1,
+                            final String startpt2,
+                            final String startpt3,
+                            final String rho) {
+
         // Creating a new document containing initial guess data.
         Document document = new Document();
 
         document.append("nvars",    nvars   )
                 .append("startpt0", startpt0)
-                .append("startpt1", startpt1)
-                .append("rho",      rho     );
+                .append("startpt1", startpt1);
+
+        if ((startpt2 != null) && (startpt3 != null)) {
+            document.append("startpt2", startpt2)
+                    .append("startpt3", startpt3);
+        }
+
+        document.append("rho", rho);
 
         // Putting initial guess data to the database.
         HookeJeevesApplication.collection.insertOne(document);
