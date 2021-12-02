31254bef095108bd3cc93497cccadc257b203c98
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2021-12-02 18:35:30 +1100

    Get rid of restricted set of scalar coordinates, naturally holding any number of them in an array.

--- src/main/java/com/minimization/nonlinear/unconstrained/hookejeeves/HookeJeevesResponsePojo.java
+++ src/main/java/com/minimization/nonlinear/unconstrained/hookejeeves/HookeJeevesResponsePojo.java
@@ -26,17 +26,8 @@ public class HookeJeevesResponsePojo {
     /** The number of variables. */
     private final int nvars;
 
-    /** The 1st starting point coordinate. */
-    private final double startpt0;
-
-    /** The 2nd starting point coordinate. */
-    private final double startpt1;
-
-    /** The 3rd starting point coordinate. */
-    private final double startpt2;
-
-    /** The 4th starting point coordinate. */
-    private final double startpt3;
+    /** The starting point coordinates. */
+    private final double[] startpt;
 
     /** The rho value. */
     private final double rho;
@@ -51,39 +42,12 @@ public class HookeJeevesResponsePojo {
     }
 
     /**
-     * The accessor method for the 1st starting point coordinate.
+     * The accessor method for the starting point coordinates.
      *
-     * @return The 1st starting point coordinate.
+     * @return The array of starting point coordinates.
      */
-    public double getStartpt0() {
-        return startpt0;
-    }
-
-    /**
-     * The accessor method for the 2nd starting point coordinate.
-     *
-     * @return The 2nd starting point coordinate.
-     */
-    public double getStartpt1() {
-        return startpt1;
-    }
-
-    /**
-     * The accessor method for the 3rd starting point coordinate.
-     *
-     * @return The 3rd starting point coordinate.
-     */
-    public double getStartpt2() {
-        return startpt2;
-    }
-
-    /**
-     * The accessor method for the 4th starting point coordinate.
-     *
-     * @return The 4th starting point coordinate.
-     */
-    public double getStartpt3() {
-        return startpt3;
+    public double[] getStartpt() {
+        return startpt;
     }
 
     /**
@@ -96,55 +60,23 @@ public class HookeJeevesResponsePojo {
     }
 
     /**
-     * Constructs the response object for two starting point coordinates.
-     * <br />
-     * <br />This is commonly effective when the objective function
-     * is the Rosenbrock's parabolic valley function.
+     * Constructs the response object.
      *
-     * @param _nvars    The number of variables.
-     * @param _startpt0 The 1st starting point coordinate.
-     * @param _startpt1 The 2nd starting point coordinate.
-     * @param _rho      The rho value.
+     * @param _nvars   The number of variables.
+     * @param _startpt The starting point coordinates.
+     * @param _rho     The rho value.
      */
-    public HookeJeevesResponsePojo(final int    _nvars,
-                                   final double _startpt0,
-                                   final double _startpt1,
-                                   final double _rho) {
+    public HookeJeevesResponsePojo(final int      _nvars,
+                                   final double[] _startpt,
+                                   final double   _rho) {
 
-        nvars    = _nvars;
-        startpt0 = _startpt0;
-        startpt1 = _startpt1;
-        startpt2 = .0;
-        startpt3 = .0;
-        rho      = _rho;
-    }
+        nvars   = _nvars;
+        startpt = new double[nvars];
+        rho     = _rho;
 
-    /**
-     * Constructs the response object for four starting point coordinates.
-     * <br />
-     * <br />This is commonly effective when the objective function
-     * is the so-called &quot;Woods&quot; function.
-     *
-     * @param _nvars    The number of variables.
-     * @param _startpt0 The 1st starting point coordinate.
-     * @param _startpt1 The 2nd starting point coordinate.
-     * @param _startpt2 The 3rd starting point coordinate.
-     * @param _startpt3 The 4th starting point coordinate.
-     * @param _rho      The rho value.
-     */
-    public HookeJeevesResponsePojo(final int    _nvars,
-                                   final double _startpt0,
-                                   final double _startpt1,
-                                   final double _startpt2,
-                                   final double _startpt3,
-                                   final double _rho) {
-
-        nvars    = _nvars;
-        startpt0 = _startpt0;
-        startpt1 = _startpt1;
-        startpt2 = _startpt2;
-        startpt3 = _startpt3;
-        rho      = _rho;
+        for (int i = 0; i < nvars; i++) {
+            startpt[i] = _startpt[i];
+        }
     }
 }
 
