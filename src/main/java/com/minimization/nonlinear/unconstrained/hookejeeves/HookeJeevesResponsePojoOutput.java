c280d982b0009915f509224cbe923782de5fde07
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2021-12-03 00:55:00 +1100

    Add "output" POJO representation, returning as part of the response POJO.

---
+++ src/main/java/com/minimization/nonlinear/unconstrained/hookejeeves/HookeJeevesResponsePojoOutput.java
@@ -0,0 +1,87 @@
+/*
+ * src/main/java/com/minimization/nonlinear/unconstrained/hookejeeves/
+ * HookeJeevesResponsePojoOutput.java
+ * ============================================================================
+ * The Hooke and Jeeves nonlinear unconstrained minimization algorithm.
+ * Microservice. Version 0.6.0
+ * ============================================================================
+ * A Spring Boot-based application, designed and intended to be run
+ * as a microservice, implementing the nonlinear unconstrained
+ * minimization algorithm of Hooke and Jeeves.
+ * ============================================================================
+ * Copyright (C) 2020-2021 Radislav (Radicchio) Golubtsov
+ *
+ * (See the LICENSE file at the top of the source tree.)
+ */
+
+package com.minimization.nonlinear.unconstrained.hookejeeves;
+
+/**
+ * The &quot;<code>output</code>&quot; POJO representation,
+ * returning as part of the response POJO.
+ *
+ * @version 0.6.0
+ * @since   0.6.0
+ */
+public class HookeJeevesResponsePojoOutput {
+    /** The number of iterations. */
+    private final int iters;
+
+    /** The ending point coordinates. */
+    private final double[] endpt;
+
+    /** The objective function value. */
+    private final double f;
+
+    /**
+     * The accessor method for the number of iterations.
+     *
+     * @return The number of iterations.
+     */
+    public int getIters() {
+        return iters;
+    }
+
+    /**
+     * The accessor method for the ending point coordinates.
+     *
+     * @return The array of ending point coordinates.
+     */
+    public double[] getEndpt() {
+        return endpt;
+    }
+
+    /**
+     * The accessor method for the objective function value.
+     *
+     * @return The objective function value.
+     */
+    public double getF() {
+        return f;
+    }
+
+    /**
+     * Constructs the &quot;<code>output</code>&quot; part
+     * of the response object.
+     *
+     * @param _nvars The number of variables.
+     * @param _iters The number of iterations.
+     * @param _endpt The ending point coordinates.
+     * @param _f     The objective function value.
+     */
+    public HookeJeevesResponsePojoOutput(final int      _nvars,
+                                         final int      _iters,
+                                         final double[] _endpt,
+                                         final double   _f) {
+
+        iters = _iters;
+        endpt = new double[_nvars];
+        f     = _f;
+
+        for (int i = 0; i < _nvars; i++) {
+            endpt[i] = _endpt[i];
+        }
+    }
+}
+
+// vim:set nu et ts=4 sw=4:
