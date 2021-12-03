a1b4d6bcf44a3013e9a0462a391f27605534795b
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2021-12-03 17:45:00 +1100

    Add POJO representation, returning in the response when erroneous behavior is occurred.

---
+++ src/main/java/com/minimization/nonlinear/unconstrained/hookejeeves/HookeJeevesResponsePojoError.java
@@ -0,0 +1,49 @@
+/*
+ * src/main/java/com/minimization/nonlinear/unconstrained/hookejeeves/
+ * HookeJeevesResponsePojoError.java
+ * ============================================================================
+ * The Hooke and Jeeves nonlinear unconstrained minimization algorithm.
+ * Microservice. Version 0.6.6
+ * ============================================================================
+ * A Spring Boot-based application, designed and intended to be run
+ * as a microservice, implementing the nonlinear unconstrained
+ * minimization algorithm of Hooke and Jeeves.
+ * ============================================================================
+ * Copyright (C) 2020-2021 Radislav (Radicchio) Golubtsov
+ *
+ * (See the LICENSE file at the top of the source tree.)
+ */
+
+package com.minimization.nonlinear.unconstrained.hookejeeves;
+
+/**
+ * The POJO representation, returning in the response
+ * when erroneous behavior is occurred.
+ *
+ * @version 0.6.6
+ * @since   0.6.6
+ */
+public class HookeJeevesResponsePojoError {
+    /** The error message. */
+    private final String error;
+
+    /**
+     * The accessor method for the error message.
+     *
+     * @return The error message.
+     */
+    public String getError() {
+        return error;
+    }
+
+    /**
+     * The effective constructor.
+     *
+     * @param _error The error message.
+     */
+    public HookeJeevesResponsePojoError(final String _error) {
+        error = _error;
+    }
+}
+
+// vim:set nu et ts=4 sw=4:
