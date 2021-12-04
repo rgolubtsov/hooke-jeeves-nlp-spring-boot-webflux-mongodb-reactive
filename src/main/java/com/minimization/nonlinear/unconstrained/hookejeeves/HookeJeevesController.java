0e57a2bfb26df757fe7814bfe28a051eb354cb86
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2021-12-05 00:30:00 +1100

    Bump version number to 0.6.9.

--- src/main/java/com/minimization/nonlinear/unconstrained/hookejeeves/HookeJeevesController.java
+++ src/main/java/com/minimization/nonlinear/unconstrained/hookejeeves/HookeJeevesController.java
@@ -3,7 +3,7 @@
  * HookeJeevesController.java
  * ============================================================================
  * The Hooke and Jeeves nonlinear unconstrained minimization algorithm.
- * Microservice. Version 0.6.7
+ * Microservice. Version 0.6.9
  * ============================================================================
  * A Spring Boot-based application, designed and intended to be run
  * as a microservice, implementing the nonlinear unconstrained
@@ -44,7 +44,7 @@ import        com.minimization.nonlinear.unconstrained.hookejeeves.algorithm.Woo
 /**
  * The controller class of the microservice.
  *
- * @version 0.6.7
+ * @version 0.6.9
  * @since   0.0.1
  */
 @RestController
